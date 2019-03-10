49780cb83c84f229300b73abae16fdec9b00c0d7
韦弦 <weixian.zy@antfin.com>
2019-03-10 20:21:19 +0800

    init commit

---
+++ parliament-paxos/src/main/java/io/github/parliament/paxos/Paxos.java
@@ -0,0 +1,16 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2019 All Rights Reserved.
+ */
+package io.github.parliament.paxos;
+
+/**
+ *
+ * @author zy
+ * @version $Id: Paxos.java, v 0.1 2019年03月08日 2:50 PM zy Exp $
+ */
+public class Paxos {
+    public void start(Proposal proposal) {
+
+    }
+}
---
+++ parliament-paxos/src/main/java/io/github/parliament/paxos/Proposer.java
@@ -0,0 +1,16 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2019 All Rights Reserved.
+ */
+package io.github.parliament.paxos;
+
+/**
+ *
+ * @author zy
+ * @version $Id: Proposer.java, v 0.1 2019年03月08日 3:17 PM zy Exp $
+ */
+public class Proposer {
+    public void propose(Proposal proposal) {
+
+    }
+}
